0c9381dc9fb37bdf4b6c1ffab38f0cce06f77af7
Bekzod Abdullojonov <bekzodabdullojonov@MacBook-Pro-2.local>
2025-12-27 17:40:01 +0500

    aaa

--- src/components/SecureContactForm.tsx
+++ src/components/SecureContactForm.tsx
@@ -254,6 +254,7 @@ const SecureContactForm: React.FC<SecureContactFormProps> = ({
                 errors.name ? "border-red-500" : "border-gray-300"
               }`}
               placeholder={t.formInputName}
+              maxLength={20}
               required
             />
           </div>
@@ -269,6 +270,7 @@ const SecureContactForm: React.FC<SecureContactFormProps> = ({
               onChange={(e) => handleInputChange("email", e.target.value)}
               className="w-full px-4 py-3 border rounded-lg focus:ring-2 focus:ring-purple-500 border-gray-300"
               placeholder="example@mail.com"
+              maxLength={50}
             />
           </div>
 
